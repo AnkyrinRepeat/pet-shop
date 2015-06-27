24605aad7674ad5a1da167c647667a45d5db7fd2
MKS-students <mks.students@hackreactor.com>
2015-06-26 19:56:37 -0500

    Shop front with data, and like functionality complete

--- client/components/PetShopWindow.js
+++ client/components/PetShopWindow.js
@@ -1,17 +1,46 @@
-var m = require('mithril')
-var Shop = require('../models/shop')
+var m = require('mithril');
+var Shop = require('../models/shop');
 
-
-var PetShopWindow = module.exports = {}
+var PetShopWindow = module.exports = {};
 
 PetShopWindow.controller = function () {
-  var ctrl = this
-  ctrl.shop = m.prop(null)
-  Shop.fetch().then(ctrl.shop)
+  var Pleb = false;
+  var ctrl = this;
+  ctrl.shop = m.prop(null);
+  ctrl.pets = m.prop(null);
+
+  Shop.fetch().then(ctrl.shop);
+  Shop.petsFetch().then(ctrl.pets);
+  // ctrl.pets is our collection of pet models
+  ctrl.like = function() {
+    this.likes.push('apitoken');
+
+  }
 }
 
+// PetSignIn.controller = function() {
+
+// }
+
 PetShopWindow.view = function (ctrl) {
-  return m('.pet-shop', [
-    m('h1', "Welcome to " + ctrl.shop().name)
-  ])
+  // if(!(Pleb)) {
+    return m('.pet-shop', [
+      m('h1', "Welcome to " + ctrl.shop().name),
+        ctrl.pets().map(function(pet){
+          return m('.pets', [
+         m('div', "Pet name: " + pet.name),
+         m('div', "Pet species: " + pet.species),
+         m('img', {src: pet.imageUrl, style: {width: '300px'}}),
+         m('div', pet.likes.length + " people like this wonderful pet!!!"),
+         m('button', {onclick : ctrl.like.bind(pet)}, 'Like'),
+         ])
+       })
+    ]);
+  // }
 }
+
+
+
+// PetSignIn.view = function (ctrl) {
+
+// }
--- client/models/shop.js
+++ client/models/shop.js
@@ -1,8 +1,23 @@
 var m = require('mithril')
 
 
-var Shop = module.exports = {}
+var Shop = module.exports = {};
 
 Shop.fetch = function () {
   return m.request({ method: 'GET', url: 'http://pet-shop.api.mks.io/shops/1' })
 }
+
+Shop.petsFetch = function() {
+  return m.request({ method: 'GET', url: 'http://pet-shop.api.mks.io/shops/1/pets' })
+}
+
+Shop.signIn = function(login) {
+  return m.request({ method: 'POST', url: 'http://pet-shop.api.mks.io/signin', data : login})
+}
+
+Shop.signUp = function(login) {
+  return m.request({ method: 'POST', url: 'http://pet-shop.api.mks.io/signup', data : login})
+}
+
+
+
