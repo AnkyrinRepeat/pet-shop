4ccfc1504731a8108d0fca4863457d8e780017fd
Colin Wiley <colinwwiley@gmail.com>
2015-06-27 10:25:36 -0500

    Signin Check

--- client/components/PetShopWindow.js
+++ client/components/PetShopWindow.js
@@ -4,8 +4,8 @@ var Shop = require('../models/shop');
 var PetShopWindow = module.exports = {};
 
 PetShopWindow.controller = function () {
-  var Pleb = false;
   var ctrl = this;
+  ctrl.Pleb = false;
   ctrl.shop = m.prop(null);
   ctrl.pets = m.prop(null);
 
@@ -23,21 +23,24 @@ PetShopWindow.controller = function () {
 // }
 
 PetShopWindow.view = function (ctrl) {
-  // if(!(Pleb)) {
-    return m('.pet-shop', [
-      m('h1', "Welcome to " + ctrl.shop().name),
+  return m('.pet-shop', [
+    m('h1', "Welcome to " + ctrl.shop().name),
+      ctrl.Pleb ? 
         ctrl.pets().map(function(pet){
           return m('.pets', [
-         m('div', "Pet name: " + pet.name),
-         m('div', "Pet species: " + pet.species),
-         m('img', {src: pet.imageUrl, style: {width: '300px'}}),
-         m('div', pet.likes.length + " people like this wonderful pet!!!"),
-         m('button', {onclick : ctrl.like.bind(pet)}, 'Like'),
-         ])
-       })
-    ]);
-  // }
+           m('div', "Pet name: " + pet.name),
+           m('div', "Pet species: " + pet.species),
+           m('img', {src: pet.imageUrl, style: {width: '300px'}}),
+           m('div', pet.likes.length + " people like this wonderful pet!!!"),
+           m('button', {onclick : ctrl.like.bind(pet)}, 'Like'),
+           ])
+        })
+      : //ternary 
+        m('p', 'You are not authorized')
+      
+  ]);
 }
+
 
 
 
